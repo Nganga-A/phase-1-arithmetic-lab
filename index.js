77b5e5d2f705ea04a4514dc699455c2b4e268be0
Abed Nganga <ngangaabed@gmail.com>
2023-06-13 10:46:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+  
+
+  let num1 =31;
+  let num2 =2;
+  const multiply = (num1 * num2);
+  console.log(multiply);
+
+  const random = Math.floor(Math.random()*10);
+  console.log(random);
+
+  let num3=14;
+  let num4=5;
+  let mod = (num3 % num4);
+  
+  let max = Math.max(2,3,4,20);
+
+  
+
+  
